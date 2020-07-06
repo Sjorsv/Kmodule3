06bed0a7a1691a21827af92f24cc08db5f5fa764
Sjors <sjorsv11@gmail.com>
2020-07-06 02:39:40 +0200

    Finish prototype
    Finish prototype

---
+++ data/animals.js
@@ -0,0 +1,62 @@
+var svgMapDataAnimals = {
+  values: {
+    NL: {
+      birds: [
+        {
+          name: 'Vos',
+          description: 'Dit is een Vos',
+          number: 100,
+          image: 'vos168x110.jpg'
+        }
+      ],
+      mammals: [
+        {
+          name: 'Everzwijn',
+          description: 'Dit is een Everzwijn',
+          number: 10,
+          image: 'everzwijn168x122.jpg'
+        }
+      ],
+      reptiles: [
+        {
+          name: 'Reptiles',
+          description: 'Dit is een Reptiel',
+          number: 10,
+          image: 'everzwijn168x122.jpg'
+        }
+      ],
+      amphibians: [
+        {
+          name: 'Kikker',
+          description: 'Dit is een Amphibie',
+          number: 10,
+          image: 'everzwijn168x122.jpg'
+        }
+      ],
+      fish: [
+        {
+          name: 'Een vis',
+          description: 'Dit is een vis',
+          number: 10,
+          image: 'everzwijn168x122.jpg'
+        }
+      ],
+      insects: [
+        {
+          name: 'Een insect',
+          description: 'Dit is een insect',
+          number: 10,
+          image: 'everzwijn168x122.jpg'
+        }
+      ]
+    },
+    BE: {
+      birds: [
+        // ...
+      ],
+      mammals: [
+        // ...
+      ]
+    }
+  }
+}
